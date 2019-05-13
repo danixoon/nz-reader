6b94b155e08aa5e38bb82eb9c97e666ce487f7ee
D <danixoon@gmail.com>
2019-05-14 00:03:33 +0300

    yeah

--- client/src/components/OrderModal.tsx
+++ client/src/components/OrderModal.tsx
@@ -23,34 +23,44 @@ class OrderModal extends React.Component<any> {
     const { correct, input } = this.state;
     return (
       // <div className="position-relative">
-      // <CSSTransition in={open} timeout={300} classNames="cart-modal" unmountOnExit>
-      // {/* <Button>ПУПААА</Button> */}
-      <Modal isOpen={open} toggle={toggle}>
-        {/* <div className="ml-auto mr-0 mt-0 position-absolute bg-white col-md-4 col-lg-3 col-100  shadow-lg" style={{ zIndex: 1050, right: 0, top: `0.5rem` }}> */}
-        <ModalHeader toggle={toggle}>Корзина</ModalHeader>
-        <ModalBody>
-          <InputValidateGroup validated={this.validated}>
-            <InputValidate idleMessage="Обращение к вам" successMessage="Отлично" name="firstName" schema={joi.string().error(e => "Обязательное поле")}>
-              <input type="lastName" className="w-100 form-control mt-2" placeholder="Иван" />
-            </InputValidate>
-            <InputValidate idleMessage="Обращение к вам" successMessage="Отлично" name="lastName" schema={joi.string().error(e => "Обязательное поле")}>
-              <input type="firstName" className="w-100 form-control mt-2" placeholder="Иванович" />
-            </InputValidate>
-            <InputValidate idleMessage="Адрес доставки" successMessage="Отлично" name="address" schema={joi.string().error(e => "Обязательное поле")}>
-              <input type="address" className="w-100 form-control mt-2" placeholder="г. Далеко, ул. Поближе, д. Здесь, кв. Тут" />
-            </InputValidate>
-            <InputValidate idleMessage="Ваша почта" successMessage="Отлично" name="email" schema={joi.string().error(e => "Обязательное поле")}>
-              <input type="email" className="w-100 form-control mt-2" placeholder="sombody@once.told" />
-            </InputValidate>
-          </InputValidateGroup>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="danger" disabled={!correct || orderStatus === "LOADING"} className="w-100" onClick={() => (orderStatus === "SUCCESS" ? toggle() : submit(input))}>
-            {orderStatus === "LOADING" ? <Spinner size="sm" color="white" /> : orderStatus === "SUCCESS" ? <Check className="text-light" /> : "Завершить"}
-          </Button>
-        </ModalFooter>
-        {/* </div> */}
-      </Modal>
+      // <CSSTransition in={open && orderStatus !== "SUCCESS"} classNames="order-modal" timeout={1000}>
+        <Modal isOpen={open} toggle={toggle}>
+          {/* <div className="ml-auto mr-0 mt-0 position-absolute bg-white col-md-4 col-lg-3 col-100  shadow-lg" style={{ zIndex: 1050, right: 0, top: `0.5rem` }}> */}
+          <ModalHeader toggle={toggle}>Корзина</ModalHeader>
+          <ModalBody>
+            <InputValidateGroup validated={this.validated}>
+              <InputValidate idleMessage="Обращение к вам" successMessage="Отлично" name="firstName" schema={joi.string().error(e => "Обязательное поле")}>
+                <input type="lastName" className="w-100 form-control mt-2" placeholder="Иван" />
+              </InputValidate>
+              <InputValidate idleMessage="Обращение к вам" successMessage="Отлично" name="lastName" schema={joi.string().error(e => "Обязательное поле")}>
+                <input type="firstName" className="w-100 form-control mt-2" placeholder="Иванович" />
+              </InputValidate>
+              <InputValidate idleMessage="Адрес доставки" successMessage="Отлично" name="address" schema={joi.string().error(e => "Обязательное поле")}>
+                <input type="address" className="w-100 form-control mt-2" placeholder="г. Далеко, ул. Поближе, д. Здесь, кв. Тут" />
+              </InputValidate>
+              <InputValidate idleMessage="Ваша почта" successMessage="Отлично" name="email" schema={joi.string().error(e => "Обязательное поле")}>
+                <input type="email" className="w-100 form-control mt-2" placeholder="sombody@once.told" />
+              </InputValidate>
+            </InputValidateGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="danger"
+              disabled={!correct || orderStatus === "LOADING"}
+              className="w-100"
+              onClick={() =>
+                orderStatus === "SUCCESS"
+                  ? toggle()
+                  : (() => {
+                      this.setState({ correct: false });
+                      submit(input);
+                    })()
+              }
+            >
+              {orderStatus === "LOADING" ? <Spinner size="sm" color="white" /> : orderStatus === "SUCCESS" ? <Check className="text-light" /> : "Завершить"}
+            </Button>
+          </ModalFooter>
+        </Modal>
       // </CSSTransition>
       // </div>
     );
